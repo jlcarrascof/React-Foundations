09dd6eaafb1ff6aec1831d17cdf1cd4506dd6afc
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-07-10 12:23:54 -0400

    Update TextInput component in App.jsx to remove unnecessary placeholders and values

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,6 @@ function App() {
   return (
     <div>
       <TextInput placeholder={"What's happening ... "} maxLength={125} />
-      <TextInput placeholder={"Tell me the news ... "} maxLength={250} />
-      <TextInput placeholder={"Tell me the news ... "} maxLength={250} value={"Anything else"} />
-      <TextInput maxLength={250} value={"Anything else"} />
     </div>
   )
 
