b1a4c26409bf99d9594eac34f2dd21519c90da2f
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-26 20:44:48 -0400

    chore: Update TextInput component to use useState for counter and add button to update counter

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,14 +1,16 @@
+import { useState } from "react";
 import styles from "./TextInput.module.css";
 
 export default function TextInput({placeholder = "What's happening ... ", maxLength, ...props}) {
 
-    let counter = 0;
+    let counter = useState(0);
     let limLength = 125;
 
     return (
         <div>
             <textarea className={styles.input} placeholder={placeholder} maxLength={maxLength} {...props} />
             <p>{counter} / {limLength}</p>
+            <button onClick={counter + 1}>Update Counter</button>
         </div>
     )
 }
