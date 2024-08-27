3487e853a9ebd2c28653ef06e10b50ac183f5b35
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-26 20:37:06 -0400

    chore: Add character counter to TextInput component

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,9 +1,14 @@
 import styles from "./TextInput.module.css";
 
 export default function TextInput({placeholder = "What's happening ... ", maxLength, ...props}) {
+
+    let counter = 0;
+    let limLength = 125;
+
     return (
         <div>
             <textarea className={styles.input} placeholder={placeholder} maxLength={maxLength} {...props} />
+            <p>{counter} / {limLength}</p>
         </div>
     )
 }
