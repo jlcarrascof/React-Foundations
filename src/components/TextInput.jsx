53adfb0b5ff2f2e8413eff93254992c9b3b71af1
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-08-26 20:48:25 -0400

    chore: Refactor TextInput component to use useState for counter and update button

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -3,14 +3,14 @@ import styles from "./TextInput.module.css";
 
 export default function TextInput({placeholder = "What's happening ... ", maxLength, ...props}) {
 
-    let counter = useState(0);
+    const [counter, setCounter] = useState(0);
     let limLength = 125;
 
     return (
         <div>
             <textarea className={styles.input} placeholder={placeholder} maxLength={maxLength} {...props} />
             <p>{counter} / {limLength}</p>
-            <button onClick={counter + 1}>Update Counter</button>
+            <button onClick={anyFunction()}>Update Counter</button>
         </div>
     )
 }
